6e6be0d01bcc4e346f52b7e54776d3a2d921f4c6
Shubh Tamrakar <166866724+Shubh-Tamrakar@users.noreply.github.com>
2026-04-27 18:24:08 +0530

    Update positionsController.js

--- backend/controller/positionsController.js
+++ backend/controller/positionsController.js
@@ -3,4 +3,5 @@ const { PositionsModel } = require("../model/PositionsModel");
 module.exports.index = async (req, res) => {
   let allPositions = await PositionsModel.find({});
   res.json(allPositions);
+  
 };
