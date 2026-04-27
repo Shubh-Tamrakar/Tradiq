7c5652dae4c942b2fe8637f38ea19706136eb5da
Shubh Tamrakar <166866724+Shubh-Tamrakar@users.noreply.github.com>
2026-04-27 18:32:09 +0530

    Update ProtectedRoute.js

--- dashboard/src/components/ProtectedRoute.js
+++ dashboard/src/components/ProtectedRoute.js
@@ -1,6 +1,7 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 
+
 export const ProtectedRoute = ({ children }) => {
   const { user } = useAuth();
   if (!user) {
