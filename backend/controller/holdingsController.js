5f490ebdf52bc519dd60d037521989e49d33d14d
Shubh Tamrakar <166866724+Shubh-Tamrakar@users.noreply.github.com>
2026-04-27 18:23:41 +0530

    Update holdingsController.js

--- backend/controller/holdingsController.js
+++ backend/controller/holdingsController.js
@@ -3,4 +3,5 @@ const { HoldingsModel } = require("../model/HoldingsModel");
 module.exports.index = async (req, res) => {
   let allHoldings = await HoldingsModel.find({});
   res.json(allHoldings);
-};
+ };
+ 
