4c0808ae1aab12f738d94e37b3959acb75aab9b5
Shubh Tamrakar <166866724+Shubh-Tamrakar@users.noreply.github.com>
2026-04-27 18:33:26 +0530

    Update WatchListAction.js

--- dashboard/src/components/WatchListAction.js
+++ dashboard/src/components/WatchListAction.js
@@ -7,6 +7,7 @@ import GeneralContext from "./GeneralContext";
 function WatchListAction({ uid }) {
   const generalContext = useContext(GeneralContext);
 
+  
   const handleBuyClick = () => {
     generalContext.openBuyWindow(uid);
   };
